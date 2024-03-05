7aaf1a4fec52ce5d6bc3ca33aa5c9c43a7b8427a
Julien Houyet <julien.houyet@gmail.com>
2024-03-05 16:34:32 +0100

    chore: add pokemonapi.sql file

---
+++ migrations/pokemonapi.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : mysql:3306
+-- Généré le : mar. 05 mars 2024 à 15:34
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `pokemonapi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20240305135723', '2024-03-05 13:57:28', 15),
+('DoctrineMigrations\\Version20240305140751', '2024-03-05 14:07:57', 34),
+('DoctrineMigrations\\Version20240305151023', '2024-03-05 15:10:26', 104);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pokemon`
+--
+
+CREATE TABLE `pokemon` (
+  `id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `height` double NOT NULL,
+  `weight` double NOT NULL,
+  `base_experience` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Déchargement des données de la table `pokemon`
+--
+
+INSERT INTO `pokemon` (`id`, `name`, `height`, `weight`, `base_experience`) VALUES
+(1, 'bulbasaur', 0.7, 6.9, 64),
+(2, 'ivysaur', 1, 13, 142),
+(3, 'venusaur', 2, 100, 236);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pokemon_type`
+--
+
+CREATE TABLE `pokemon_type` (
+  `pokemon_id` int NOT NULL,
+  `type_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Déchargement des données de la table `pokemon_type`
+--
+
+INSERT INTO `pokemon_type` (`pokemon_id`, `type_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(2, 2),
+(3, 1),
+(3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type`
+--
+
+CREATE TABLE `type` (
+  `id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Déchargement des données de la table `type`
+--
+
+INSERT INTO `type` (`id`, `name`) VALUES
+(1, 'grass'),
+(2, 'poison');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `pokemon`
+--
+ALTER TABLE `pokemon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `pokemon_type`
+--
+ALTER TABLE `pokemon_type`
+  ADD PRIMARY KEY (`pokemon_id`,`type_id`),
+  ADD KEY `IDX_B077296A2FE71C3E` (`pokemon_id`),
+  ADD KEY `IDX_B077296AC54C8C93` (`type_id`);
+
+--
+-- Index pour la table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `pokemon`
+--
+ALTER TABLE `pokemon`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `pokemon_type`
+--
+ALTER TABLE `pokemon_type`
+  ADD CONSTRAINT `FK_B077296A2FE71C3E` FOREIGN KEY (`pokemon_id`) REFERENCES `pokemon` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_B077296AC54C8C93` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
