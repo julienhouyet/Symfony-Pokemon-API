a24d7258fbb03f6be33f4ccbd93b8a91db65fbd9
julienhouyet <julien.houyet@gmail.com>
2024-03-07 11:05:44 +0100

    feat: access token authenticator

---
+++ src/Security/ApiTokenHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\ApiTokenRepository;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+
+class ApiTokenHandler implements AccessTokenHandlerInterface
+{
+	public function __construct(private ApiTokenRepository $apiTokenRepository)
+	{
+	}
+
+	public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge
+	{
+		$token = $this->apiTokenRepository->findOneBy(['token' => $accessToken]);
+
+		if (!$token) {
+			throw new BadCredentialsException();
+		}
+
+		if (!$token->isValid()) {
+			throw new CustomUserMessageAuthenticationException('Token expired');
+		}
+
+		return new UserBadge($token->getOwnedBy()->getUserIdentifier());
+	}
+}
