e8a4e0e856e0881022bf9d0fb6d198b4c21419ca
julienhouyet <julien.houyet@gmail.com>
2024-03-14 07:11:25 +0100

    feat: first try docker prod

--- docker/prod/Dockerfile.php
+++ docker/prod/Dockerfile.php
@@ -3,17 +3,17 @@ FROM php:8.2-fpm
 
 # Installez les dépendances requises pour Symfony
 RUN apt-get update && apt-get install -y \
-        git \
-        unzip \
-        libzip-dev \
-        libicu-dev \
-		locales \
-    && docker-php-ext-install \
-        pdo \
-        pdo_mysql \
-        zip \
-        intl \
-    && apt-get clean && rm -rf /var/lib/apt/lists/*
+	git \
+	unzip \
+	libzip-dev \
+	libicu-dev \
+	locales \
+	&& docker-php-ext-install \
+	pdo \
+	pdo_mysql \
+	zip \
+	intl \
+	&& apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Ajout des locales FR
 RUN locale-gen fr_FR.UTF-8
@@ -25,6 +25,10 @@ WORKDIR /var/www/symfony
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 ENV COMPOSER_ALLOW_SUPERUSER=1
+
+# Installer Symfony CLI
+RUN curl -sS https://get.symfony.com/cli/installer | bash && \
+	mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
 
 # Copier le reste du code source en premier
 COPY . /var/www/symfony
